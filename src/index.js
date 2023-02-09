5bc449c4b6c2ffd7e10df7d4289093ff1f04d5ba
Gwenaëlle Herissant <gh@pointligneplan.fr>
2023-02-09 17:27:33 +0100

    [react] Added router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+//import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import Root from './routes/Root';
 import reportWebVitals from './reportWebVitals';
+import Profile from './pages/Profile';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Profile />
+      }, 
+      {
+        path: '/user/:id',
+        element: <Profile />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
