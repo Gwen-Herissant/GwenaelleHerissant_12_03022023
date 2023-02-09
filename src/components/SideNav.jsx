af730b024c5e1e42bbd920a74d2d27f4ba96de51
Gwenaëlle Herissant <gh@pointligneplan.fr>
2023-02-09 17:29:35 +0100

    [react/styles] SideNav component with styles

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,41 @@
+import icon_01 from "../assets/images/sidenav-icon-01.svg"
+import icon_02 from "../assets/images/sidenav-icon-02.svg"
+import icon_03 from "../assets/images/sidenav-icon-03.svg"
+import icon_04 from "../assets/images/sidenav-icon-04.svg"
+
+import "../styles/sidenav.css"
+
+export default function SideNav(){
+  return(
+    <div className="sidenav">
+      <nav className="sidenav_nav">
+        <ul>
+          <li>
+            <a href="#">
+              <img src={icon_01} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={icon_02} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={icon_03} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={icon_04} alt="" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <p className="sidenav_text">Copyright, SportSee 2020</p>
+    </div>
+  );
+}
+
+
+
