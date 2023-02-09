5466bfc5dca33820904dca78d374622ca5a69b8c
Gwenaëlle Herissant <gh@pointligneplan.fr>
2023-02-09 17:29:21 +0100

    [react/styles] Header component with styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import logo from "../assets/images/sportsee-logo.svg"
+import '../styles/header.css'
+
+export default function Header(){
+  return(
+    <header className="header">
+      <nav>
+        <ul>
+          <li>
+            <a href="#">
+              <img src={logo} alt="Sportsee Logo" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Accueil
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Profil
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Réglage
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Communauté
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
