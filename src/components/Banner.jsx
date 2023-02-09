1da66ef775185c0f1ff38a5f7afb1968553b8c1d
Gwenaëlle Herissant <gh@pointligneplan.fr>
2023-02-09 17:29:54 +0100

    [react/styles] Banner component with styles

---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+import "../styles/banner.css"
+
+export default function Banner(){
+  return(
+    <div className="banner">
+      <h1>Bonjour <span>[prénom]</span></h1>
+      <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+    </div>
+  );
+}
