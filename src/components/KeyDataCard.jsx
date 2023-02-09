09ff498cb1afdb85f3fcaad7040fc9eb7462c9f5
Gwenaëlle Herissant <gh@pointligneplan.fr>
2023-02-09 17:30:24 +0100

    [react/styles] Keydata component with styles (missing data)

---
+++ src/components/KeyDataCard.jsx
@@ -0,0 +1,13 @@
+import "../styles/keydata.css";
+
+export default function KeyData(){
+  return(
+    <div className="keydata">
+      <img src="#" alt="" />
+      <div className="keydata_text-container">
+        <p className="keydata_text-data">[1,930kCal]</p>
+        <p className="keydata_text-description">Calories</p>
+      </div>
+    </div>
+  );
+}
